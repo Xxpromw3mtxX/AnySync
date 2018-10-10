839329883b7ccf4bdfa4c6c64b0143d72d3ab7f7
unknown <fra.metta@hotmail.it>
2018-10-10 15:15:15 +0200

    Login form partially working

--- AnySync-Java/src/anysync/java/Login.java
+++ AnySync-Java/src/anysync/java/Login.java
@@ -18,40 +18,85 @@
 package anysync.java;
 
 import java.awt.Dimension;
-import java.awt.GraphicsConfiguration;
 import java.awt.event.ActionListener;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
-import com.apple.eawt.Application;
+//import com.apple.eawt.Application;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import javax.imageio.ImageIO;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 /**
  *
  * @author Francesco
  * @version 1
  */
-public class Login extends JFrame{
+public class Login extends JFrame implements ActionListener {
     //Attributes
     private int client_id = 1231; //AnySync application ID
     private String response_type = "token";
     private String token;
     String url = "https://anilist.co/api/v2/oauth/authorize?client_id=" + client_id + "&response_type=" + response_type;
-    static GraphicsConfiguration gc;
+    private JPanel panel;
+    private JTextField username;
+    private JButton anilogin; 
+    private BufferedImage anylogo;
+    private JLabel anylogoin;
     
-    public Login (String title){
-        super(title);
-        ImageIcon img = new ImageIcon("src/res/icon.png");
-        Application application = Application.getApplication();
-        Image image = Toolkit.getDefaultToolkit().getImage("src/res/icon.png");
-        setPreferredSize(new Dimension(300, 250));
+    /**
+     * CONSTRUCTOR
+     */
+    public Login ()throws IOException{
+        super();
+        this.panel = new JPanel();
+        this.username = new JTextField(20);
+        this.anilogin = new JButton("Login with AniList");
+        this.anylogo = ImageIO.read(new File("src/res/login-form-logo.png"));
+        this.anylogoin = new JLabel(new ImageIcon(anylogo));
+        build();  
+        actions();
+    }
+    
+    // Create the window
+    private void build(){        
+        add(panel);
+        init();
         setDefaultCloseOperation(3);
-        setResizable(false);
+        setPreferredSize(new Dimension(300, 180));
+        ImageIcon img = new ImageIcon("src/res/icon.png");
+//      Application application = Application.getApplication();
+        Image image = Toolkit.getDefaultToolkit().getImage("src/res/icon.png");
         setIconImage(img.getImage());
-        application.setDockIconImage(image);
+//      application.setDockIconImage(image);
+        setResizable(false);
+        setTitle("AnySync.v1 - Login");
         pack();
         setLocationRelativeTo(null);
         setVisible(true);
     }
     
+    // Add things to the window
+    private void init(){
+        panel.add(anylogoin);
+        panel.add(username);
+        username.getText();
+        panel.add(anilogin);
+    }
+    
+    //Action listener
+    private void actions() {
+        anilogin.addActionListener(this);
+    }
+    @Override
+    public void actionPerformed(ActionEvent act) {
+        
+    }    
 }
