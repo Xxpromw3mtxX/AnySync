8fc5b8c65103b46aadee48093a4b416e294807b3
Xxpromw3mtxX <fra.metta@hotmail.it>
2018-10-12 11:17:12 +0200

    Added ActionListener and DocumentListener working

--- AnySync-Java/src/anysync/java/Login.java
+++ AnySync-Java/src/anysync/java/Login.java
@@ -24,6 +24,8 @@ import javax.swing.JFrame;
 import com.apple.eawt.Application;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
@@ -43,12 +45,13 @@ import javax.swing.text.Document;
  * @author Francesco
  * @version 1
  */
-public class Login extends JFrame implements DocumentListener{
+public class Login extends JFrame implements DocumentListener, ActionListener{
     //Attributes
     private int client_id = 1231; //AnySync application ID
-    private String response_type = "token";
+    private String redirectURI;
+    private String response_type = "code";
     private String token;
-    private String url = "https://anilist.co/api/v2/oauth/authorize?client_id=" + client_id + "&response_type=" + response_type;
+    private String url;
     private URI finalurl;
     private JPanel panel;
     private JTextField username;
@@ -63,7 +66,8 @@ public class Login extends JFrame implements DocumentListener{
      */
     public Login ()throws IOException, URISyntaxException{
         super();
-        this.finalurl = new URI("https://anilist.co/api/v2/oauth/authorize?client_id=" + client_id + "&response_type=" + response_type);
+        this.redirectURI = new String("https://anilist.co/api/v2/oauth/pin");
+        this.finalurl = new URI("https://anilist.co/api/v2/oauth/authorize?client_id=" + client_id + "&redirect_uri=" + redirectURI + "&response_type=" + response_type);
         this.panel = new JPanel();
         this.username = new JTextField(15);
         this.anilogin = new JButton("Login with AniList");
@@ -71,12 +75,13 @@ public class Login extends JFrame implements DocumentListener{
         this.anylogoin = new JLabel(new ImageIcon(anylogo));
         this.browser = Desktop.getDesktop();
         this.textFieldDoc = username.getDocument();
+        
         build();  
         actions();
     }
     
     // Create the window
-    private void build(){        
+    private void build() throws IOException{        
         add(panel);
         init();
         setDefaultCloseOperation(3);
@@ -94,7 +99,7 @@ public class Login extends JFrame implements DocumentListener{
     }
     
     // Add things to the window
-    private void init(){
+    private void init() throws IOException{
         panel.add(anylogoin);
         panel.add(username);
         username.getText();
@@ -104,11 +109,12 @@ public class Login extends JFrame implements DocumentListener{
     }
     
     //Checking function
-    public void changed() {
+    public void changed() throws IOException {
         if (username.getText().equals("")){
             anilogin.setEnabled(false);
         }else{
             anilogin.setEnabled(true);
+            
         }
     }
     
@@ -116,15 +122,26 @@ public class Login extends JFrame implements DocumentListener{
     private void actions() {
         username.getDocument().addDocumentListener(new DocumentListener() {
             public void changedUpdate(DocumentEvent e) {
-                changed();
+                try {
+                    changed();
+                } catch (IOException ex) {
+                }
             }
             public void removeUpdate(DocumentEvent e) {
-                changed();
+                try {
+                    changed();
+                } catch (IOException ex) {
+                }
             }
             public void insertUpdate(DocumentEvent e) {
-                changed();
+                try {
+                    changed();
+                } catch (IOException ex) {
+                }
             }
         });
+        
+        anilogin.addActionListener(this);
     }
     
     //Ignore
@@ -141,5 +158,17 @@ public class Login extends JFrame implements DocumentListener{
     @Override
     public void changedUpdate(DocumentEvent e) {
         throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent act) {
+        anilogin.addActionListener( new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                try{ 
+                    browser.browse(finalurl);
+                }catch(IOException err){
+                }
+            }
+        });
     }
 }
