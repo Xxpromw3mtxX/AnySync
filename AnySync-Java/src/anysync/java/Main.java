b75d8daa68c282e59a8d77287bf1bea5ffc9ac05
unknown <fra.metta@hotmail.it>
2018-10-12 16:08:38 +0200

    Main corrected

--- AnySync-Java/src/anysync/java/Main.java
+++ AnySync-Java/src/anysync/java/Main.java
@@ -31,7 +31,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException, URISyntaxException {
-        // gui for the login form 
+        //Login form
         Login form = new Login();
        
     }
