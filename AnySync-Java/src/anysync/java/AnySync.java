ced7b5e278726c7e432d09a44d1214ab932c29f6
unknown <fra.metta@hotmail.it>
2018-10-12 20:16:30 +0200

    AnySync menubar added

--- AnySync-Java/src/anysync/java/AnySync.java
+++ AnySync-Java/src/anysync/java/AnySync.java
@@ -22,35 +22,52 @@ package anysync.java;
  * Disabled on windows version
 */
 //import com.apple.eawt.Application; 
+import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Image;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 
 /**
  * @author Francesco
  * @version 1
  */
-public class AnySync extends JFrame{
+public class AnySync extends JFrame implements ActionListener{
     //Attributes
     private JPanel panel;
     
+    //Menu attributes
+    private JMenuBar menubar;
+    private JMenu menu;
+    private JMenuItem item1;
     /**
      * CONSTRUCTOR
      */
     public AnySync ()throws IOException{
         this.panel = new JPanel();
         
+        //Menu creation
+        this.menubar = new JMenuBar();
+        this.menu = new JMenu("File");
+        this.item1 = new JMenuItem ("Exit");
         
         build();
+        actions();
+        
     }
     
     // Create the window
     private void build() throws IOException{
         add(panel);
+        init();
         setDefaultCloseOperation(3);
         setPreferredSize(new Dimension(900, 600));
         ImageIcon img = new ImageIcon("src/res/icon.png");
@@ -59,6 +76,7 @@ public class AnySync extends JFrame{
         setIconImage(img.getImage());
 //        application.setDockIconImage(image); //Set Apple dock icon
         setResizable(false);
+        //setJMenuBar(menubar);
         setTitle("AnySync (Pre-Alpha)");
         pack();
         setLocationRelativeTo(null);
@@ -67,5 +85,27 @@ public class AnySync extends JFrame{
     
     // Add things to the window
     private void init() throws IOException{
+        //Add menu to menu bar
+        menubar.add(menu);
+        //Add items to menu
+        menu.add(item1);
+        //Add items to panel
+        panel.setLayout(new BorderLayout());
+        panel.add(menubar, BorderLayout.NORTH);
+    }
+    
+    //ActionListener
+    private void actions() {
+        item1.addActionListener(this);
+    }
+    
+    @Override
+    public void actionPerformed(ActionEvent act) {
+        item1.addActionListener( new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                System.exit(0);
+            }
+        });
+        
     }
 }
