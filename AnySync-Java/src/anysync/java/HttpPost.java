5f6adf4030b125e5afc31572af2ce0dd164029af
Xxpromw3mtxX <fra.metta@hotmail.it>
2018-10-26 16:17:50 +0200

    Started working on http class + senPost() class created

--- AnySync-Java/src/anysync/java/HttpPost.java
+++ AnySync-Java/src/anysync/java/HttpPost.java
@@ -17,12 +17,168 @@
 */
 package anysync.java;
 
+/**
+ * Apple library import. 
+ * Disabled on windows version
+*/
+//import com.apple.eawt.Application; 
+import java.awt.Dimension;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.image.BufferedImage;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.imageio.ImageIO;
+import javax.net.ssl.HttpsURLConnection;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 
 /**
- * @author Francesco
+ * @author Francesco Metta
  * @version 1
  */
 public class HttpPost extends JFrame{
+    //Attributes
+    private JPanel panel;
+    private JTextField auth_pin_field;
+    private BufferedImage anylogo;
+    private JLabel anylogoin;
+    private JButton httprequest;
+    public String auth_pin;
+    private String url;
+    private URL finalurl;
+    private String secret;
+    private String access_token;
+    private BufferedReader in;
+    private HttpsURLConnection spost;
+    private StringBuffer response;
+    /**
+     * CONSTRUCTOR
+     */
+    public HttpPost() throws IOException, URISyntaxException {
+        Login variables = new Login();
+        this.panel = new JPanel();
+        this.auth_pin_field = new JTextField(15);
+        this.anylogo = ImageIO.read(new File("src/res/login-form-logo.png"));
+        this.anylogoin = new JLabel(new ImageIcon(anylogo));
+        this.httprequest = new JButton("Login request");
+        this.auth_pin = new String();
+        this.secret = new String("2sTj9Pv0YAVwFGKqILHGM1Fuqkv6pZ9DRIlurPqg");
+        this.url = new String("https://anilist.co/api/v2/oauth/"
+                + "token?grant_type=authorization_code&client_id=" + variables.client_id 
+                + "&client_secret=" + secret + "&redirect_uri=" + variables.redirectURI 
+                + "&code=" + auth_pin_field.toString());
+        this.finalurl = new URL(url);
+        this.spost = (HttpsURLConnection) finalurl.openConnection();
+        this.in = new BufferedReader(new InputStreamReader(spost.getInputStream()));
+        this.response = new StringBuffer();
+        build();
+        actions();
+    }
     
+    // Create the window
+    private void build() throws IOException{
+        //Adding the panel to the program
+        add(panel);
+        init();
+        setDefaultCloseOperation(3);
+        setPreferredSize(new Dimension(300, 180));
+        ImageIcon img = new ImageIcon("src/res/icon.png");
+//        Application application = Application.getApplication(); //Apple icon
+        Image image = Toolkit.getDefaultToolkit().getImage("src/res/icon.png");
+        setIconImage(img.getImage());
+//        application.setDockIconImage(image); //Set Apple dock icon
+        setResizable(false);
+        setTitle("AnySync - Pin Auth(Pre-Alpha)");
+        pack();
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+    
+    // Add things to the window
+    private void init() throws IOException{
+        //Adding logo
+        panel.add(anylogoin);
+        //Adding the textfield to the program
+        panel.add(auth_pin_field);
+        //Adding the button
+        panel.add(httprequest);
+        //Checking
+        changed();
+    }
+    
+    //Checking function
+    public void changed() throws IOException {
+        if (auth_pin_field.getText().equals("")){
+            httprequest.setEnabled(false);
+        }else{
+            httprequest.setEnabled(true);
+        }
+    }
+    
+    // HTTP POST request
+    private void sendPost() throws Exception {
+        //add reuqest header
+        spost.setRequestMethod("POST");
+        spost.setRequestProperty("Content-Type", "application/json");
+        spost.setRequestProperty("Accept", "application/json");         
+        while ((access_token = in.readLine()) != null) {
+            response.append(access_token);
+        }
+        in.close();
+    }
+    
+    //DocumentListener + ActionListener
+    private void actions() {
+        auth_pin_field.getDocument().addDocumentListener(new DocumentListener() {
+            public void changedUpdate(DocumentEvent e) {
+                try {
+                    changed();
+                } catch (IOException ex) {
+                }
+            }
+            public void removeUpdate(DocumentEvent e) {
+                try {
+                    changed();
+                } catch (IOException ex) {
+                }
+            }
+            public void insertUpdate(DocumentEvent e) {
+                try {
+                    changed();
+                } catch (IOException ex) {
+                }
+            }
+        });
+        
+        httprequest.addActionListener( new ActionListener(){
+            public void actionPerformed(ActionEvent e){
+                auth_pin_field.setEnabled(false);
+                if(auth_pin_field.getText().equals("")){ 
+                }else{
+                    httprequest.setEnabled(false);
+                    try {
+                        sendPost();
+                        //disableME();
+                    } catch (Exception ex) {
+                    }
+                }
+            }
+        });
+    }
 }
